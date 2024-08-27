2c5a22b248a30b0d1b08b6ff1d7d61c2cd27d895
Mateo Barrios <xd2006@gmail.com>
2024-08-27 20:57:04 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'moch-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ng-app-moving-church';
+}
