189b141a483adca644eae9c4b4585f6b628233e7
Mateo Barrios <xd2006@gmail.com>
2024-10-12 01:53:35 -0300

    feat: criado componente loading, implementado interceptor e loadingService, aplicado ajuste no componente de mensagem

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LogoComponent } from './components/logo/logo.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { GoToHomeComponent } from './components/go-to-home/go-to-home.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 const ComponentList: any[] = [
   LogoComponent,
   MenuComponent,
-  GoToHomeComponent
+  GoToHomeComponent,
+  LoadingComponent
 ]
 
 @NgModule({
